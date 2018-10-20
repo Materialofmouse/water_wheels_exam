0f4c28d0e6839f5ff7935444c147eeffe6b87351
Materialofmouse <materialofmouse@gmail.com>
2018-10-20 10:34:31 +0900

    wrote main.py

--- main.py
+++ main.py
@@ -1,26 +1,51 @@
 import csv
+from datetime import datetime
+import os
 #import Adafruit_ADS1x15 as ads
 #import RPi.GPIO as gpio
 
 #outside tempracture
 def get_temp():
+  temp = 10
   return temp
 
 #water tempracture
 def get_wt_temp():
+  temp = 20
   return temp
 
 #cpu tempracture
 def get_cpu_temp():
+  temp = 50
   return temp
 
+def get_wt_level():
+  level = 22
+  return level
 
 def control_water_level(level):
+  return
+
+path = str(os.getcwd()) + '/data/' + str(datetime.now().strftime('%Y/%m/%d'))
+
+
+if __name__ == '__main__':
+
+  #make dir from date
+  if not (os.path.isdir(path)):
+    os.makedirs(path)
+
+  f = open(path + '/' + str(datetime.now().strftime('%H:%M:%S')) + '.csv','w')
   
+  data = []
+  data.append(str(datetime.now().strftime('%Y/%m/%d %H:%M:%S')))
+  data.append(str(get_temp()))
+  data.append(str(get_wt_temp()))
+  data.append(str(get_cpu_temp()))
+  data.append(str(get_wt_level()))
+  
+  #write data to csv file
+  writer = csv.writer(f,lineterminator='\n')
+  writer.writerow(data)
 
-f = open('test.csv','w')
-
-writer = csv.writer(f,linterminator='\n')
-writer.writerow(data)
-
-f.close()
+  f.close()
