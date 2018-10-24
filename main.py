9157ca31f0c1efd98acebb0748d256422b97b628
Materialofmouse <materialofmouse@gmail.com>
2018-10-24 04:04:51 +0000

    update main.py get water level and water tempracture

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
+import os
 import csv
 from datetime import datetime
-import os
-#import Adafruit_ADS1x15 as ads
-#import RPi.GPIO as gpio
 
-#outside tempracture
+import Adafruit_ADS1x15
+import RPi.GPIO as gpio
+import subprocess
+
+
 def get_temp():
   temp = 10
   return temp
 
-#water tempracture
+  #water tempracture
 def get_wt_temp():
-  temp = 20
-  return temp
+  return int(subprocess.check_output(['cat','/sys/class/hwmon/hwmon0/temp1_input']))
 
-#cpu tempracture
+  #cpu tempracture
 def get_cpu_temp():
   temp = 50
   return temp
 
 def get_wt_level():
-  level = 22
-  return level
+  return Adafruit_ADS1x15.ADS1115().read_adc(2,gain=1)
 
-def control_water_level(level):
-  return
+def control_water_level():
+  return 10
 
+
+
+  #outside tempracture
+  
+#output data path
 path = str(os.getcwd()) + '/data/' + str(datetime.now().strftime('%Y/%m/%d'))
 
 
@@ -39,7 +44,6 @@ if __name__ == '__main__':
   #make file from date 
   f = open(path + '/' + str(datetime.now().strftime('%H:%M:%S')) + '.csv','w')
   writer = csv.writer(f,lineterminator='\n')
-  
   
   data = []
   data.append(str(datetime.now().strftime('%H:%M:%S')))
