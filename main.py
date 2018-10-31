ef470122f7ce9a70eded868d61f88c51cdbc80aa
Materialofmouse <materialofmouse@gmail.com>
2018-10-31 03:04:35 +0000

    update main.py

--- main.py
+++ main.py
@@ -2,35 +2,69 @@
 import os
 import csv
 from datetime import datetime
+import threading
 
 #hardware
 import Adafruit_ADS1x15
 import RPi.GPIO as gpio
 import subprocess
 
+gpio.setmode(GPIO.BCM)
+gpio.setup(16,gpio.OUT)#down pomp
+gpio.setup(20,gpio.OUT)#up pomp
+
+#high 
+gpio.output(16,True)
+gpio.output(20,True)
 
 def get_temp():
   temp = 10
   return temp
 
-  #water tempracture
+#water tempracture return int
 def get_wt_temp():
   return float(subprocess.check_output(['cat','/sys/class/hwmon/hwmon0/temp1_input'])) / 100
 
-  #cpu tempracture
+#cpu tempracture return int
 def get_cpu_temp():
   temp = 50
   return temp
 
+#get water level,return int(0.5cm)
 def get_wt_level():
-  return Adafruit_ADS1x15.ADS1115().read_adc(2,gain=1)
+  volt = Adafruit_ADS1x15.ADS1115().read_adc(2,gain=1)
+  level = volt * 2.048 / 32768
+  return level / 0.066
 
-def control_water_level():
-  return 10
+#update later
+def control_water_level(level):
+  
+  #control
+  level_now = get_wt_level()
+  while level_now == level:
+    
+    #up control
+    if level_now > level:
+      gpio.output(16,False)
+    
+    #donw control
+    else:
+      gpio.output(20,False)
+      time.sleep(sec)
+      gpio.output(20,False)
+    
+    #update now water level
+    level_now = get_wt_level()
 
+  return level_now
 
+def input_water_level():
+  print("input the watar level")
+  water_level = input()
+  sec = input()
+  return control_water_level(watar_level,sec)
 
-  #outside tempracture
+#outside tempracture
   
 #output data path
 path = str(os.getcwd()) + '/data/' + str(datetime.now().strftime('%Y/%m/%d'))
@@ -43,18 +77,25 @@ if __name__ == '__main__':
     os.makedirs(path)
   
   
-  #make file from date 
-  f = open(path + '/' + str(datetime.now().strftime('%H:%M:%S')) + '.csv','w')
-  writer = csv.writer(f,lineterminator='\n')
-  
-  data = []
-  data.append(str(datetime.now().strftime('%H:%M:%S')))
-  data.append(str(get_temp()))
-  data.append(str(get_wt_temp()))
-  data.append(str(get_cpu_temp()))
-  data.append(str(get_wt_level()))
-  
-  #write data to csv file
-  writer.writerow(data)
+  while True:
+    
+    #make file from date 
+    f = open(path + '/' + str(datetime.now().strftime('%H:%M:%S')) + '.csv','w')
+    writer = csv.writer(f,lineterminator='\n')
+    
+    
+    thread = threading.Thread(target=control_water_level)
 
-  f.close()
+    data = []
+    data.append(str(datetime.now().strftime('%H:%M:%S')))
+    data.append(str(get_temp()))
+    data.append(str(get_wt_temp()))
+    data.append(str(get_cpu_temp()))
+    data.append(str(get_wt_level()))
+    
+    #write data to csv file
+    writer.writerow(data)
+
+    f.close()
+
+
--- sensors.py
+++ sensors.py
@@ -2,12 +2,13 @@ import Adafruit_ADS1x15 as ads
 import time
 
 CHANNEL = 0
-GAIN = 2/3
-I = 0.0016
+GAIN = 2
+#16bit 
 adc = ads.ADS1115()
 
 while True:
-  v = float(adc.read_adc(CHANNEL, gain=GAIN)) * 6.144 / 32768
-  level = (1500 - (v / 0.0016)) / 56
-  print(str(level) + ' cm')
+  v = float(adc.read_adc(CHANNEL, gain=GAIN)) * 2.048 / 32768 - 2.464
+  level = v / 0.33
+  print(str(round(level,1)) + ' cm')
+  
   time.sleep(0.5)
