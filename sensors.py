1baf296d9e6bdf23b5f29ce901cebc104a57bcd6
Materialofmouse <materialofmouse@gmail.com>
2018-11-07 08:32:00 +0000

    update sensors.py

--- sensors.py
+++ sensors.py
@@ -8,7 +8,7 @@ adc = ads.ADS1115()
 
 while True:
   v = float(adc.read_adc(CHANNEL, gain=GAIN)) * 2.048 / 32768
-  level = ((v)-0.75)
+  level = 21.03 - (((v / 21.4) * 1000 - 26.4) / 2.35)
   print(str(round(level,2)) + ' cm')
   
   time.sleep(0.5)
