31138bea7d6733fcd78de085b760d92eb084e523
Materialofmouse <materialofmouse@gmail.com>
2018-12-27 16:06:49 +0900

    created new main

---
+++ c_main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import RPi.GPIO as GPIO
+import time
+
+class sensors():
+  def __init__(self):
+    level_pin = [18,23,24]
+
+    GPIO.setwarnings(False)
+    GPIO.setup(GPIO.BCM)
+    GPIO.cleanup()
+
+    #water sensor
+    GPIO.setup(level_pin,GPIO.IN)
+
+    #sonic sensor
+    TRIG = 14
+    ECHO = 15
+    GPIO.setup(TRIG,GPIO.OUT)
+    GPIO.setup(ECHO,GPIO.OUT)
+
+  def get_water_level(self):
+    level = 0
+    for i in range(self.level_pin):
+      level += (not GPIO.input(i))
+
+    return level
+
+  def get_rpm(self):
+
+
