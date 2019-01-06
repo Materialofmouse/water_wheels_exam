a7c51d2846bd6bd3e7dc608e63f7f6f1dd7b7f49
Materialofmouse <materialofmouse@gmail.com>
2019-01-06 10:50:42 +0900

    acquired value from temperature sensor and implemented data export

--- t.py
+++ t.py
@@ -1,8 +1,13 @@
 import threading
 import queue
 import RPi.GPIO as GPIO
-import datetime
+from datetime import datetime
 import time
+import sys
+import csv
+import pytz
+import os
+import subprocess
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -17,26 +22,35 @@ q = queue.Queue()
 def set_water_level():
   global level_pin
   level = [0] * 2
-
+  
   for i,pin_num in enumerate(level_pin):
     for pin in pin_num:
       level[i] += not GPIO.input(pin)
    
   global q
-  q.put(dict(level1=level[0], level2=level[1]))
+  q.put(dict(level1=level[0] + 1, level2=level[1] + 1))
   print('[level] --- ok')
+
 
 def set_water_temp():
   #--- get water level ---
   global q
-  q.put(dict(temp = 20.5))
-  print('[temp] --- ok')
+  t = subprocess.check_output(['cat','/sys/class/hwmon/hwmon0/temp1_input'])
+  q.put(dict(temp = t))
+  print('[temp]  --- ok')
 
 def on_timer():
-  time.sleep(10)
-  print('10s') 
+  time.sleep(2)
 
 def main():
+  
+  JST = pytz.timezone('Asia/Tokyo')
+  filepath = str('/home/pi/data/' + str(datetime.now(JST).strftime('%Y/%m/%d')))
+
+  if not (os.path.isdir(filepath)):
+    os.makedirs(filepath)
+  writepath = filepath + '/' + str(datetime.now(JST).strftime('%H:%M:%S')) + '.csv'
+
   while True:
     threadlist = list()
     threadlist.append(threading.Thread(target=set_water_level()))
@@ -52,9 +66,19 @@ def main():
     data = q.get()
     while not q.empty():
       data.update(q.get())
-      
-
+    
     print(data)
+    f = open(writepath,'a')
+    writer = csv.writer(f,lineterminator='\n')
+
+    writedata = []
+    writedata.append(str(datetime.now(JST).strftime('%H:%M:%S')))
+    writedata.append(str(data['temp']))
+    writedata.append(str(data['level1']))
+    writedata.append(str(data['level2']))
+
+    writer.writerow(writedata)
+    f.close()
 
   ## --- kaki komi syori ---
 
